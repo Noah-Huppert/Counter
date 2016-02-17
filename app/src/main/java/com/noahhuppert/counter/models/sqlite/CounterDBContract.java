fabc0fedc8e289ae4539feaf7e106f5880e42a9c
Noah Huppert <noahhuppert@gmail.com>
2016-02-17 16:21:11 -0500

    Started implementation of SQLite db in app

--- app/src/main/java/com/noahhuppert/counter/models/sqlite/CounterDBContract.java
+++ app/src/main/java/com/noahhuppert/counter/models/sqlite/CounterDBContract.java
@@ -9,7 +9,29 @@ public final class CounterDBContract {
         public static final String TABLE_NAME = "counters";
 
         public static final String COLUMN_NAME_NAME = "name";
-        public static final String COLUMN_NAME_CREATION_DATE = "creation_date";
-        //public static final String
+        public static final String COLUMN_NAME_CREATION_DATE_TIME = "creation_date_time";
+    }
+
+    public static abstract class CounterSessionEntry implements BaseColumns {
+        public static final String TABLE_NAME = "counter_sessions";
+
+        public static final String COLUMN_NAME_COUNTER_ID = "counter_id";
+        public static final String COLUMN_NAME_CREATION_DATE_TIME = "creation_date_time";
+    }
+
+    public static abstract class CounterSegmentEntry implements BaseColumns {
+        public static final String TABLE_NAME = "counter_segments";
+
+        public static final String COLUMN_NAME_COUNTER_SESSION_ID = "counter_session_id";
+        public static final String COLUMN_NAME_CREATION_DATE_TIME = "creation_date_time";
+        public static final String COLUMN_NAME_DATA = "data";
+    }
+
+    public static abstract class CountEntry implements BaseColumns {
+        public static final String TABLE_NAME = "counts";
+
+        public static final String COLUMN_NAME_COUNTER_SEGMENT_ID = "counter_segment_id";
+        public static final String COLUMN_NAME_DATA_TIME = "date_time";
     }
 }
+
--- app/src/main/java/com/noahhuppert/counter/models/sqlite/CounterDBOpenHelper.java
+++ app/src/main/java/com/noahhuppert/counter/models/sqlite/CounterDBOpenHelper.java
@@ -1,13 +1,20 @@
 package com.noahhuppert.counter.models.sqlite;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class CounterDBOpenHelper extends SQLiteOpenHelper {
-    private static final int DB_VERSION = 0;
+    public static final int DATABASE_VERSION = 0;
+    public static final String DATABASE_NAME = "Counter.db";
+
+    public CounterDBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        CounterDBMigrations.Migration_None_To_0(db);
     }
 
     @Override
